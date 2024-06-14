db9234304804111c5ba3a98cdbddfac28d75aac6
Thati05 <m.seithati01@gmail.com>
2024-06-14 19:47:11 +0200

    commit

--- app/(folder)/about/page.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const About = () => {
-  return (
-    <div className=' justify-items-center flex it'>About</div>
-  )
-}
-
-export default About
---
+++ app/[user]/page.js
@@ -0,0 +1,8 @@
+'use client';
+
+const User = ({ params }) => {
+  console.log(params);
+  return <div>User</div>;
+};
+
+export default User;
