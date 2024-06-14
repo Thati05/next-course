1cd1c3a6be73c96f2eb3f5343f1e8341549c4da2
Thati05 <m.seithati01@gmail.com>
2024-06-14 18:39:21 +0200

    commit

--- app/(folder)/about/page.jsx
+++ app/(folder)/about/page.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div className=' justify-items-center flex it'>About</div>
   )
 }
 
--- app/page.js
+++ app/page.js
@@ -1,11 +1,25 @@
 /*This is the main page (home page ) */
-
-import React from 'react'
+'use client';
+import { useRouter } from 'next/navigation';
+import React from 'react';
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+  const router = useRouter();
 
-export default Home
+  /*We can create a function  */
+  const nevigate = (page) => {
+    router.push(page)/// specify the page (page is a parameter) the we want to nevigate to
+  }
+
+  return (
+    <section>
+      <h1>useRouter</h1>
+      <button onClick={() => nevigate("/about")}>
+        Go to the about page
+      </button>
+    </section>
+    /*This will lead to the about page  */
+  );
+};
+
+export default Home;
