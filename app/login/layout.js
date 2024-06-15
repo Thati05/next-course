7107fdfb93b7b5bf808078437e0844df4188268f
Thati05 <m.seithati01@gmail.com>
2024-06-15 10:07:08 +0200

    commit

--- app/login/layout.js
+++ app/login/layout.js
@@ -1,8 +1,22 @@
+import Link from 'next/link'
 import React from 'react'
 
 const Layout = ({ children }) => {
   return (
-    <div>layout</div>
+    <div>
+      <ul>
+        <li>
+          <Link href="/login">Main login</Link>
+        </li>
+        <li>
+          <Link href="/login/user">User</Link>
+        </li>
+        <li>
+          <Link href="/login/admin">Admin</Link>
+        </li>
+      </ul>
+      {children}
+    </div>
   )
 }
 
