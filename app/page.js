2a390c7ef63c96b7d590bc64a08b954f3b9c72bb
Thati05 <m.seithati01@gmail.com>
2024-06-16 21:01:57 +0200

    commit

--- app/page.js
+++ app/page.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from 'react';
+import Image from 'next/image';
 
 const Hello = () => {
   return (
-    <div>Hello</div>
-  )
-}
+    <div>
+      Hello
+      <Image
+        width={100}
+        height={100}
+        src='https://images.unsplash.com/photo-1592818868295-f527dbac420d?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+        alt='Abba'
+      />
+    </div>
+  );
+};
 
-export default Hello
+export default Hello;
