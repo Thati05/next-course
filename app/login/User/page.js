70d3cf29c7c1521e23b337eb5c9c0a62c0e3f394
Thati05 <m.seithati01@gmail.com>
2024-06-16 20:40:24 +0200

    commit

--- app/login/User/page.js
+++ app/login/User/page.js
@@ -1,9 +1,13 @@
-import React from 'react'
+
+import Image from "next/image"
 
 const user = () => {
   return (
-    <div>user</div>
+    <div>
+      <Image scr='https://unsplash.com/photos/body-of-water-oQC81OHcl4Q' />
+    </div>
   )
 }
 
 export default user
+
